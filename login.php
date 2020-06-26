d80ee1367e5a74103fbb820069ab8e1a1fb723b8
hanifabd <hanifabd23@gmail.com>
2020-06-26 14:59:36 +0700

    login

---
+++ login.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Arsip Surat | Dinkes Melawi</title>
+    <!-- plugins:css -->
+    <link rel="stylesheet" href="css/materialdesignicons.min.css">
+    <link rel="stylesheet" href="css/vendor.bundle.base.css">
+    <!-- endinject -->
+    <!-- Plugin css for this page -->
+    <!-- End plugin css for this page -->
+    <!-- inject:css -->
+    <!-- endinject -->
+    <!-- Layout styles -->
+    <link rel="stylesheet" href="css/style.css">
+    <!-- End layout styles -->
+    <link rel="shortcut icon" href="images/favicon.ico" />
+  </head>
+  <body>
+    <div class="container-scroller overflow-hidden">
+      <div class="container-fluid page-body-wrapper full-page-wrapper">
+        <div class="content-wrapper d-flex align-items-center auth shadow">
+          <div class="row flex-grow">
+            <div class="col-lg-10 mx-auto">
+              <div class="auth-form-light log-grid-container">
+                <div class="log-grid-item">
+                  <div class="p-4 text-left">
+                    <img class="log-img mr-2" src="images/dinkes/dinas.svg" alt="logo" />
+                    <span class="m-0">Dinkes Melawi</span>
+                  </div>
+                  <div class="text-left p-c-5">
+                    <form class="p-3">
+                      <div class="form-group">
+                        <label class="text-secondary" for="username">Email</label>
+                        <input id="username" type="email" class="form-control form-control-lg text-primary rounded-pill" placeholder="Masukan email disini...">
+                      </div>
+                      <div class="form-group">
+                        <label class="text-secondary" for="pass">Password</label>
+                        <input id="pass" type="password" class="form-control form-control-lg text-primary rounded-pill" placeholder="Kalau password disini ya...">
+                      </div>
+                      <div class="mt-5 text-right">
+                        <a class="rounded-pill btn mt-3 btn-block btn-gradient-primary btn-lg font-weight-medium auth-form-btn" href="#">Masuk</a>
+                      </div>
+                      <div class="mt-5">
+                        <hr>
+                      </div>
+                      <div class="mt-3 text-center">
+                        <a class="h6 text-secondary" href="#">Tidak bisa masuk ?</a>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+                <div class="log-grid-item hidden-mb">
+                  <div class="h-100 w-100">
+                    <div class="illust-img-hero h-100">
+                      <span class="position-absolute sos m-2 text-secondary">image by tubik</span>
+                      <img class="hero-img" src="images/dinkes/hero.png" alt="">
+                    </div>
+                  </div>
+                </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- content-wrapper ends -->
+      </div>
+      <!-- page-body-wrapper ends -->
+    </div>
+    <!-- container-scroller -->
+    <!-- plugins:js -->
+    <script src="js/vendor.bundle.base.js"></script>
+    <!-- endinject -->
+    <!-- Plugin js for this page -->
+    <!-- End plugin js for this page -->
+    <!-- inject:js -->
+    <script src="js/off-canvas.js"></script>
+    <script src="js/hoverable-collapse.js"></script>
+    <script src="js/misc.js"></script>
+    <!-- endinject -->
+  </body>
+</html>
